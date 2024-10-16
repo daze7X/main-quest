3ea0d28a6d45b88bb257617600f94a452e5df3a2
daze7X <150314615+daze7X@users.noreply.github.com>
2024-10-16 20:00:22 +0700

    OOP(class dan atribut)

---
+++ motor.py
@@ -0,0 +1,9 @@
+class motor:
+    name = ""
+    roda = 0
+
+motor_trail = motor()
+motor_trail.roda = 2
+motor_trail.name = "Supra Bapak"
+
+print(f"name: {motor_trail.name}\nrodas: {motor_trail.roda}")
